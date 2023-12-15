6442641a9efaf4d51bf14f8f4a9dea7e9a5f7aa7
Octavio Luperón <armando2445@outlook.com>
2023-12-15 11:32:29 -0400

    Going forward

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  public title: string = 'Hola mundo';
-  public counter: number = 10;
-
-  increaseBy( value: number ):void {
-    this.counter += value
-  }
-
-  reset( value: number ):void {
-    this.counter = value
-  }
-
-  decreaseBy(value: number):void {
-    this.counter += value
-  }
 
 }
